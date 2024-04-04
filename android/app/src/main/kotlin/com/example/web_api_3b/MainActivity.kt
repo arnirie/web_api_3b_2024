9d7fd81660ed9640101a7674868b2d0506c7fdbc
Arni-Rie Tamayo <arnirietamayo@gmail.com>
2024-04-04 13:59:12 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/web_api_3b/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.web_api_3b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:web_api_3b/screens/register.dart';
+
+void main() {
+  runApp(const WebAPI());
+}
+
+class WebAPI extends StatelessWidget {
+  const WebAPI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegisterScreen(),
+    );
+  }
+}
